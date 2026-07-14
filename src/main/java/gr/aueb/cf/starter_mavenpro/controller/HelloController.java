96abb62fc020788ff0d90f53c0f6e8a1cda9633d
makisragkousis <makisragkousis@gmail.com>
2026-07-14 20:02:29 +0300

    feat: add a third version of the original method in HelloController

--- src/main/java/gr/aueb/cf/starter_mavenpro/controller/HelloController.java
+++ src/main/java/gr/aueb/cf/starter_mavenpro/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/starter-maven")
@@ -11,13 +12,20 @@ public class HelloController {
 
     @GetMapping("/v1/hello")
     public String hello(Model model) {
-        model.addAttribute("message", "Hello Maven!");
+        model.addAttribute("message", "Hello Maven!!!");
         return "index";
     }
 
+    //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @GetMapping("/v2/hello")
     public String hello2(Model model) {
-        model.addAttribute("message", "Hello Coding!");
+        model.addAttribute("message", "Hello Coding!!!");
+        return "index";
+    }
+
+    @GetMapping("/v3/hello")
+    public String hello3(Model model) {
+        model.addAttribute("message", "Hello Coding Factory!!!");
         return "index";
     }
 }
